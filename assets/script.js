b58b3d854e64a135d00b4c17b4012eac73c42c6d
Elaine Martinez <elainemartinez@Elaines-MacBook-Air.local>
2023-06-05 20:36:37 -0700

    fixed stylesheet link issue

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 //TODO: Add code to display the current date in the header of the page.
-var Date = dayjs().format('MMMM D YYYY hh:mm:ss');
-$("#currentDay").html(Date);
+var time = dayjs().format('MMMM D, YYYY hh:mm:ss');
+$("#currentDay").html(time);
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
@@ -20,11 +20,12 @@ $(document).ready(function() {
   })
   
   function getTime() {
-    var currentTime = dayjs().format('H');
+    var currentTime = parseInt(dayjs().format('H'));
+      console.log(currentTime);
     
     $(".time-block").each(function () {
       var blockTime = parseInt($(this).attr("id"));
-      
+        console.log(blockTime);
       
       if (blockTime < currentTime) {
         $(this).removeClass("future");
@@ -50,15 +51,15 @@ $(document).ready(function() {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   
-  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-  $("#hour-10 .description").val(localStorage.getItem("hour-10")); 
-  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  $("#9 .description").val(localStorage.getItem("hour-9"));
+  $("#10 .description").val(localStorage.getItem("hour-10")); 
+  $("#11 .description").val(localStorage.getItem("hour-11"));
+  $("#12 .description").val(localStorage.getItem("hour-12"));
+  $("#13 .description").val(localStorage.getItem("hour-13"));
+  $("#14 .description").val(localStorage.getItem("hour-14"));
+  $("#15 .description").val(localStorage.getItem("hour-15"));
+  $("#16 .description").val(localStorage.getItem("hour-16"));
+  $("#17 .description").val(localStorage.getItem("hour-17"));
   
   getTime();
   
