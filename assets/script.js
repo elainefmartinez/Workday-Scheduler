670b6434c68cd4e496ac739c838180f71c6a2d88
Elaine Martinez <elainemartinez@Elaines-MacBook-Air.local>
2023-06-05 21:02:33 -0700

    updated psuedo code comments

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-//TODO: Add code to display the current date in the header of the page.
+//TODO: Add code to display the current date in the header of the page. (Added current month date year, and time)
 var time = dayjs().format('MMMM D, YYYY hh:mm:ss');
 $("#currentDay").html(time);
 
@@ -26,54 +26,57 @@ $(document).ready(function() {
     $(".time-block").each(function () {
       var blockTime = parseInt($(this).attr("id"));
         console.log(blockTime);
-      
-      if (blockTime < currentTime) {
-        $(this).removeClass("future");
-        $(this).removeClass("present");
-        $(this).addClass("past");
-      }
-      else if (blockTime === currentTime) {
-        $(this).removeClass("past");
-        $(this).removeClass("future");
-        $(this).addClass("present");
-      }
-      else {
-        $(this).removeClass("present");
-        $(this).removeClass("past");
-        $(this).addClass("future");
-      }
-      
-    });
-  }
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+        if (blockTime < currentTime) {
+          $(this).removeClass("future");
+          $(this).removeClass("present");
+          $(this).addClass("past");
+        }
+        else if (blockTime === currentTime) {
+          $(this).removeClass("past");
+          $(this).removeClass("future");
+          $(this).addClass("present");
+        }
+        else {
+          $(this).removeClass("present");
+          $(this).removeClass("past");
+          $(this).addClass("future");
+        }
+        
+      });
+    }
+    // TODO: Add code to apply the past, present, or future class to each time
+    // block by comparing the id to the current hour. HINTS: How can the id
+    // attribute of each time-block be used to conditionally add or remove the
+    // past, present, and future classes? How can Day.js be used to get the
+    // current hour in 24-hour time?
+    
+    
+    $("#9 .description").val(localStorage.getItem("hour-9"));
+    $("#10 .description").val(localStorage.getItem("hour-10")); 
+    $("#11 .description").val(localStorage.getItem("hour-11"));
+    $("#12 .description").val(localStorage.getItem("hour-12"));
+    $("#13 .description").val(localStorage.getItem("hour-13"));
+    $("#14 .description").val(localStorage.getItem("hour-14"));
+    $("#15 .description").val(localStorage.getItem("hour-15"));
+    $("#16 .description").val(localStorage.getItem("hour-16"));
+    $("#17 .description").val(localStorage.getItem("hour-17"));
+    
+    getTime();
+    
+  })
   
-  $("#9 .description").val(localStorage.getItem("hour-9"));
-  $("#10 .description").val(localStorage.getItem("hour-10")); 
-  $("#11 .description").val(localStorage.getItem("hour-11"));
-  $("#12 .description").val(localStorage.getItem("hour-12"));
-  $("#13 .description").val(localStorage.getItem("hour-13"));
-  $("#14 .description").val(localStorage.getItem("hour-14"));
-  $("#15 .description").val(localStorage.getItem("hour-15"));
-  $("#16 .description").val(localStorage.getItem("hour-16"));
-  $("#17 .description").val(localStorage.getItem("hour-17"));
+  //HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
   
-  getTime();
   
-})
+  
+  
+  //used examples from wk5(ex:18,24) for add and remove class
+//used related structure of locaStorage from this weeks miniproject code and parseInt
+//https://day.js.org/docs/en/display/format for formatting time display
 
-//HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
 
 
-
-  
-
-  
-  
  
